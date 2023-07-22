4bc384388afbbca454d91f0a3b08c1c9a2994119
Lokesh Sonawane <omsonawane15203@gmail.com>
2023-07-22 08:14:03 +0530

    Climbing stairs using MEMOIZATION

--- src/_20_Dynamic_Programming/_2_Climbing_Stairs.java
+++ src/_20_Dynamic_Programming/_2_Climbing_Stairs.java
@@ -1,7 +1,10 @@
 package _20_Dynamic_Programming;
 
+import java.lang.reflect.Array;
+
 public class _2_Climbing_Stairs {
 
+    // Normal RECURSION - O(2^n)
     public static int countStairs(int n){
         if(n==0){           // For not climbing there is "1" way i.e. not to do anything 🤣
             return 1;
@@ -13,9 +16,29 @@ public class _2_Climbing_Stairs {
         return countStairs(n-1) + countStairs(n-2);
     }
 
+    //      MEMOIZATION   -    O(n)
+    public static int memoStairs(int n){
+        int[] dp = new int[n+1];
+        if (n == 0){
+            return 1;
+        }
+        if (n < 0){
+            return 0;
+        }
+        dp[n] = memoStairs(n-1) + memoStairs(n-2);
+
+        return dp[n];
+    }
+
+    //
+
     public static void main(String[] args) {
 
         int n = 5;
-        System.out.println(countStairs(n));
+//        System.out.println(countStairs(n));
+
+
+        System.out.println(memoStairs(n));
+
     }
 }
