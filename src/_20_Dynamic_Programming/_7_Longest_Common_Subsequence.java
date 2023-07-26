9ea30fedadbf9c0ead7b2d08e7f38a3c6bfc4186
Lokesh Sonawane <omsonawane15203@gmail.com>
2023-07-26 11:30:38 +0530

    Longest common subsequence Using Memoization

--- src/_20_Dynamic_Programming/_7_Longest_Common_Subsequence.java
+++ src/_20_Dynamic_Programming/_7_Longest_Common_Subsequence.java
@@ -2,22 +2,41 @@ package _20_Dynamic_Programming;
 
 public class _7_Longest_Common_Subsequence {
 
-    public static int lcs(String s1, String s2, int n, int m){
+    // Recursion
+//    public static int lcs(String s1, String s2, int n, int m){
+//        if(n == 0 || m == 0){
+//            return 0;
+//        }
+//
+//        if (s1.charAt(n-1) == s2.charAt(m-1)){
+//            return lcs(s1, s2, n-1, m-1) + 1;
+//        }
+//        else{
+//            int ans1 = lcs(s1, s2, n-1, m);
+//            int ans2 = lcs(s1, s2, n, m-1);
+//            return Math.max(ans1, ans2);
+//        }
+//    }
+
+    //  Memoization - O(n*m)
+    public static int lcs2(String s1, String s2, int n, int m, int[][] dp){
         if(n == 0 || m == 0){
             return 0;
         }
 
-        if (s1.charAt(n-1) == s2.charAt(m-1)){
-            return lcs(s1, s2, n-1, m-1) + 1;
+        if (dp[n][m] != -1){
+            return dp[n][m];        // this avoids the recalculation for the same values of n amd m if already calculated
+        }
+
+        if (s1.charAt(n-1) == s2.charAt(m-1)){  // if last character same
+            return dp[n][m] = lcs2(s1, s2, n-1, m-1, dp) + 1;
         }
         else{
-            int ans1 = lcs(s1, s2, n-1, m);
-            int ans2 = lcs(s1, s2, n, m-1);
-            return Math.max(ans1, ans2);
+            int ans1 = lcs2(s1, s2, n-1, m, dp);
+            int ans2 = lcs2(s1, s2, n, m-1, dp);
+            return dp[n][m] = Math.max(ans1, ans2);
         }
     }
-
-
 
     public static void main(String[] args) {
 
@@ -26,6 +45,13 @@ public class _7_Longest_Common_Subsequence {
         int n = str1.length();
         int m = str2.length();
 
-        System.out.println(lcs(str1, str2, n, m));
+//        System.out.println(lcs(str1, str2, n, m));
+        int[][] dp = new int[n+1][m+1];
+        for (int i=0; i<n+1; i++){
+            for (int j=0; j<m+1; j++){
+                dp[i][j] = -1;
+            }
+        }
+        System.out.println(lcs2(str1, str2, n, m, dp));
     }
 }
