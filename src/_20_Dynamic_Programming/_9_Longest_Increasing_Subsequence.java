fb57eac36f5f4b6e95910a4b5e7e3ea9a1258a82
Lokesh Sonawane <omsonawane15203@gmail.com>
2023-07-27 21:30:50 +0530

    Longest Increasing Subsequence using Tabulation.

--- src/_20_Dynamic_Programming/_9_Longest_Increasing_Subsequence.java
+++ src/_20_Dynamic_Programming/_9_Longest_Increasing_Subsequence.java
@@ -1,5 +1,8 @@
 package _20_Dynamic_Programming;
 
+import java.util.Arrays;
+import java.util.HashSet;
+
 public class _9_Longest_Increasing_Subsequence {
 
     // LIS using Recursion - Written by me.
@@ -16,10 +19,45 @@ public class _9_Longest_Increasing_Subsequence {
     }
 
 
+    public static int lisTab(int[] arr, int n, int m, int[][] dp){
+        HashSet<Integer> hs = new HashSet<>(); // To remove duplicate entries. in arr
+        for (int i=0; i<arr.length; i++){
+            hs.add(arr[i]);
+        }
+
+        int[] arr2 = new int[arr.length];
+        int p=0;                            // here p is just a variable to replace i;
+        for (int num : hs) {       // This is a foreach loop
+            arr2[p] = num;
+            p++;
+        }
+        Arrays.sort(arr2);
+
+        //  Now     ###    LCS     ###
+        // initializing first row and col to zero;
+
+        for (int i=1; i<n+1; i++){
+            for (int j=1; j<m+1; j++){
+                if(arr[i-1] == arr2[j-1]){
+                    dp[i][j] = dp[i-1][j-1] + 1;
+                }else {
+                    int ans1 = dp[i-1][j];
+                    int ans2 = dp[i][j-1];
+                    dp[i][j] = Math.max(ans1, ans2);
+                }
+            }
+        }
+        return dp[n][m];
+    }
+
+
 
     public static void main(String[] args) {
         int[] arr = {50, 3, 10, 7, 40, 80};
-        int n = arr.length;
-        System.out.println(lis(arr, n));
+//        int n = arr.length;                      // For
+//        System.out.println(lis(arr, n));         // Recursion
+        int n = arr.length, m = arr.length;
+        int dp[][] = new int[n+1][m+1];
+        System.out.println(lisTab(arr, n, m, dp));
     }
 }
