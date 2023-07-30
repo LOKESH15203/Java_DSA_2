95c763c7bbaaa4327d87e731725b39d776836f05
Lokesh Sonawane <omsonawane15203@gmail.com>
2023-07-30 17:12:25 +0530

    Counting bst when No. of nodes are given -> Change a line to make it more readable according to the question. Both the methods are 100% correct.

--- src/_20_Dynamic_Programming/_12_Counting_BSTs.java
+++ src/_20_Dynamic_Programming/_12_Counting_BSTs.java
@@ -8,7 +8,10 @@ public class _12_Counting_BSTs {
 
         for (int i=2; i<=n; i++){
             for (int j=0; j<i; j++){
-                dp[i] += dp[j] * dp[i-j-1];
+//                dp[i] += dp[j] * dp[i-j-1]; // This line or for better understanding the 3 lines below
+                int left = dp[j];
+                int right = dp[i-j-1];
+                dp[i] += left * right;
             }
         }
         return dp[n];
