7cd77c5b09e0ed55453ee312d0dc478005caac17
Lokesh Sonawane <omsonawane15203@gmail.com>
2023-07-30 16:36:40 +0530

    Calculating n th CATALAN Number using TABULATION.

--- src/_20_Dynamic_Programming/_11_Catalan_Number.java
+++ src/_20_Dynamic_Programming/_11_Catalan_Number.java
@@ -13,6 +13,7 @@ public class _11_Catalan_Number {
         return ans;
     }
 
+    // Memoization
     public static int catMemo(int n, int[] dp){
         if(n == 0 || n == 1){
             return 1;
@@ -29,14 +30,28 @@ public class _11_Catalan_Number {
         return dp[n] = ans;
     }
 
+//  Tabulation - O(n^2) - this is still far better than the recursion code.
+    public static int catTab(int n, int[] dp){
+        dp[0] = 1;
+        dp[1] = 1;
+        for (int i=2; i<=n; i++){
+            for (int j=0; j<i; j++){         // this calculates Ci for above loop;
+                dp[i] += dp[j] * dp[i-j-1];   // Ci = Cj * Ci-j-1
+            }
+        }
+        return dp[n];
+    }
+
     public static void main(String[] args) {
 
-        int n = 30;
-        int[] dp = new int[n+1];
-        for (int i=0; i<dp.length; i++){
-            dp[i] = -1;
-        }
-        System.out.println(catMemo(n, dp));
+        int n = 4;
+//        int[] dp = new int[n+1];             // For.
+//        for (int i=0; i<dp.length; i++){     // Memoization.
+//            dp[i] = -1;
+//        }
+//        System.out.println(catMemo(n, dp));
 
+        int[] dp = new int[n+1];
+        System.out.println(catTab(n, dp));
     }
 }
