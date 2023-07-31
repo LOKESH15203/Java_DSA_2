66026558303a8e8f5bde49dd434ccb9da29e6b81
Lokesh Sonawane <omsonawane15203@gmail.com>
2023-07-31 11:27:03 +0530

    Matrix Multiplication Find the least cost to multiply the No. of Matrices given in the array Using - Memoization.

--- src/_20_Dynamic_Programming/_13_Matrix_Multiplication.java
+++ src/_20_Dynamic_Programming/_13_Matrix_Multiplication.java
@@ -18,13 +18,39 @@ public class _13_Matrix_Multiplication {
         return ans;
     }
 
+    // Memoization - O(n^2)
+    public static int mcmMemo(int[] arr, int i, int j, int[][] dp){
+        if(i == j){
+            return 0;
+        }
+        if(dp[i][j] != -1){
+            return dp[i][j];
+        }
+        int ans = Integer.MAX_VALUE;
+        for (int k=i; k<j; k++){
+            int cost1 = mcmMemo(arr, i, k, dp);
+            int cost2 = mcmMemo(arr, k+1, j, dp);
+            int cost3 = arr[i-1] * arr[k] * arr[j];
+            int finalCost = cost1 + cost2 + cost3;
+            ans = Math.min(ans, finalCost);
+        }
+        return dp[i][j] = ans;
+    }
 
     public static void main(String[] args) {
 
         int[] arr = {1, 2, 3, 4, 3};
         int n = arr.length;
 
-        System.out.println(mcmRec(arr, 1, n-1));
+//        System.out.println(mcmRec(arr, 1, n-1));
+
+        int[][] dp = new int[n][n];
+        for (int i=0; i<n; i++){
+            for (int j=0; j<n; j++){
+                dp[i][j] = -1;
+            }
+        }
+        System.out.println(mcmMemo(arr, 1, n-1, dp));
 
     }
 
