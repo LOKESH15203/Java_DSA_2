653c8ed000f544f9c71dbd04fb0c2ce64b6c9263
Lokesh Sonawane <omsonawane15203@gmail.com>
2023-08-08 10:46:48 +0530

    Sting Palindrome Question with new corner cases.

--- src/_3_Strings/_1_Palindrome.java
+++ src/_3_Strings/_1_Palindrome.java
@@ -3,6 +3,31 @@ package _3_Strings;
 import java.net.Socket;
 
 public class _1_Palindrome {
+    /*
+    A phrase is a palindrome if, after converting all uppercase letters into lowercase letters and removing all non-alphanumeric characters, it reads the same forward and backward. Alphanumeric characters include letters and numbers.
+
+Given a string s, return true if it is a palindrome, or false otherwise.
+
+     */
+
+    public static boolean LeetPalindrome(String s) {
+        if (s.isEmpty()){
+            return true;
+        }
+        s = s.toLowerCase().replaceAll("[^a-zA-Z0-9]", "");
+
+        int start = 0;
+        int end = s.length()-1;
+        while(start <= end){
+            if (s.charAt(start) != s.charAt(end)){
+                return false;
+            }
+            start++;
+            end--;
+        }
+        return true;
+    }
+
 
     // Can be also done using for loop in DSA course
     public static boolean isPalindrome(String str){
