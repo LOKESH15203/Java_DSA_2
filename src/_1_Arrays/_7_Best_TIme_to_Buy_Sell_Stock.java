51aa12754181143d142251d0d2082a6afe02506c
Lokesh Sonawane <omsonawane15203@gmail.com>
2023-08-09 07:19:51 +0530

    Best time to Buy and Sell Stock - Done in Alpha Course and LEETcode's best Solution  -- Optimised Code

--- src/_1_Arrays/_7_Best_TIme_to_Buy_Sell_Stock.java
+++ src/_1_Arrays/_7_Best_TIme_to_Buy_Sell_Stock.java
@@ -23,6 +23,24 @@ public class _7_Best_TIme_to_Buy_Sell_Stock {
         return maxProfit;
     }
 
+    // Done in Alpha Course and LEETcode's best Solution  -- Optimised Code
+    public static int leetProfit(int[] prices){
+            int lsf = Integer.MAX_VALUE; // least so far
+            int op = 0; // overall profit
+            int pist = 0; // profit if sold today
+
+            for(int i = 0; i < prices.length; i++){
+                if(prices[i] < lsf){ // if we found new buy value which is more smaller then previous one
+                    lsf = prices[i]; // update our least so far
+                }
+                pist = prices[i] - lsf; // calculating profit if sold today by, Buy - sell
+                if(op < pist){ // if pist is more then our previous overall profit
+                    op = pist; // update overall profit
+                }
+            }
+            return op; // return op
+    }
+
 
     public static void main(String[] args) {
         int[] prices = {7, 1, 5, 3, 6, 4};
