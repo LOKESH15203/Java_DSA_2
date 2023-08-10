06ad3fea765c89cfd1c0232ebab569c7ffa56a35
Lokesh Sonawane <omsonawane15203@gmail.com>
2023-08-10 12:13:35 +0530

    Product Except self Optimised

--- src/_1_Arrays/_10_Product_Except_Self.java
+++ src/_1_Arrays/_10_Product_Except_Self.java
@@ -17,20 +17,42 @@ The Time Complexity would be O(n), but we are now using Auxilary Space of O(n) (
      */
 
     // Not Optimised
+//    public static int[] productExceptSelf(int[] nums) {
+//        int n= nums.length;
+//        int[] ans = new int[n];
+//        Arrays.fill(ans, 1);
+//        for (int i=0; i<n; i++){
+//            for (int j=0; j<n; j++){
+//                if (j != i){
+//                    ans[i] *= nums[j];
+//                }
+//            }
+//        }
+//        return ans;
+//    }
+
     public static int[] productExceptSelf(int[] nums) {
         int n= nums.length;
-        int[] ans = new int[n];
-        Arrays.fill(ans, 1);
-        for (int i=0; i<n; i++){
-            for (int j=0; j<n; j++){
-                if (j != i){
-                    ans[i] *= nums[j];
-                }
-            }
+        int[] pre = new int[n];
+        int[] suff = new int[n];
+        // Base case
+        pre[0] = 1;
+        suff[n-1] = 1;
+
+        for (int i=1; i<n; i++){
+            pre[i] = pre[i-1] * nums[i-1];     // gives [1, 1, 2, 6]
         }
+        for (int i=n-2; i>=0; i--){
+            suff[i] = suff[i+1] * nums[i+1];   // gives [24, 12, 8, 6]
+        }
+
+        int[] ans = new int[n];
+        for (int i=0; i<n; i++){
+            ans[i] = pre[i]*suff[i];
+        }
+
         return ans;
     }
-
 
 
     public static void main(String[] args) {
